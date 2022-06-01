5c876046c25b4118c66f8e12488da210a70508b1
= <sydanny15@gmail.com>
2022-06-01 13:38:40 -0400

    adding fake tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,75 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// Fake data taken from initial-tweets.json
+const data = [
+  {
+    user: {
+      name: "Newton",
+      avatars: "https://i.imgur.com/73hZDYK.png",
+      handle: "@SirIsaac",
+    },
+    content: {
+      text: "If I have seen further it is by standing on the shoulders of giants",
+    },
+    created_at: 1461116232227,
+  },
+  {
+    user: {
+      name: "Descartes",
+      avatars: "https://i.imgur.com/nlhLi3I.png",
+      handle: "@rd",
+    },
+    content: {
+      text: "Je pense , donc je suis",
+    },
+    created_at: 1461113959088,
+  },
+];
+
+const renderTweets = function (tweets) {
+  // loops through tweets
+  tweets.forEach((tweet) => {
+    // calls createTweetElement for each tweet
+    let tweetReturned = createTweetElement(tweet);
+    // takes return value and appends it to the tweets container
+    $("#tweets-container").append(tweetReturned);
+  });
+};
+
+const createTweetElement = function (tweet) {
+  // let $tweet = /* Your code for creating the tweet element */
+  // ...
+  // return $tweet;
+  // console.log("the tweet is:\n" + tweet.user.name + tweet.user.avatars + tweet.user.handle + " " + tweet.content.text + " " + tweet.created_at);
+
+  let tweet_text = `     <article class='tweets'>
+  <header class='tweet-header'>
+ 
+   <div class='tweet-user'>
+ <img src="${tweet.user.avatars}">
+    <div>name</div>
+    </div> 
+  <div class='user-handle'>${tweet.user.handle}</div>
+
+  </header>
+
+  <p class='tweet-body'> 
+    ${tweet.content.text}
+  </p>
+
+  <footer class='tweet-footer'>
+    <div>
+     days ago 
+    </div>
+    <div class= 'tweet-reactions'>
+    <i class="fa-brands fa-font-awesome"></i>
+    <i class="fa-solid fa-repeat"></i>
+    <i class="fa-solid fa-heart"></i>
+    </div>  
+  </footer>
+</article>
+  `;
+  // console.log(tweet_text);
+  return tweet_text;
+};
