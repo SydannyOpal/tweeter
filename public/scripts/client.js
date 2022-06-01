2ee4a6965f5804ca6cfadfeed0f251be4fb5bcbe
= <sydanny15@gmail.com>
2022-06-01 16:48:46 -0400

    fetching tweets with ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,30 +5,30 @@
  */
 
 // Fake data taken from initial-tweets.json
-const data = [
-  {
-    user: {
-      name: "Newton",
-      avatars: "https://i.imgur.com/73hZDYK.png",
-      handle: "@SirIsaac",
-    },
-    content: {
-      text: "If I have seen further it is by standing on the shoulders of giants",
-    },
-    created_at: 1461116232227,
-  },
-  {
-    user: {
-      name: "Descartes",
-      avatars: "https://i.imgur.com/nlhLi3I.png",
-      handle: "@rd",
-    },
-    content: {
-      text: "Je pense , donc je suis",
-    },
-    created_at: 1461113959088,
-  },
-];
+// const data = [
+//   {
+//     user: {
+//       name: "Newton",
+//       avatars: "https://i.imgur.com/73hZDYK.png",
+//       handle: "@SirIsaac",
+//     },
+//     content: {
+//       text: "If I have seen further it is by standing on the shoulders of giants",
+//     },
+//     created_at: 1461116232227,
+//   },
+//   {
+//     user: {
+//       name: "Descartes",
+//       avatars: "https://i.imgur.com/nlhLi3I.png",
+//       handle: "@rd",
+//     },
+//     content: {
+//       text: "Je pense , donc je suis",
+//     },
+//     created_at: 1461113959088,
+//   },
+// ];
 
 const renderTweets = function (tweets) {
   // loops through tweets
@@ -36,7 +36,7 @@ const renderTweets = function (tweets) {
     // calls createTweetElement for each tweet
     let tweetReturned = createTweetElement(tweet);
     // takes return value and appends it to the tweets container
-    $("#tweets-container").append(tweetReturned);
+    $("#tweets-container").prepend(tweetReturned);
   });
 };
 
@@ -46,12 +46,12 @@ const createTweetElement = function (tweet) {
   // return $tweet;
   // console.log("the tweet is:\n" + tweet.user.name + tweet.user.avatars + tweet.user.handle + " " + tweet.content.text + " " + tweet.created_at);
 
-  let tweet_text = `     <article class='tweets'>
+  let tweet_text = `<article class='tweets'>
   <header class='tweet-header'>
  
    <div class='tweet-user'>
  <img src="${tweet.user.avatars}">
-    <div>name</div>
+    <div>${tweet.user.name}</div>
     </div> 
   <div class='user-handle'>${tweet.user.handle}</div>
 
@@ -63,7 +63,7 @@ const createTweetElement = function (tweet) {
 
   <footer class='tweet-footer'>
     <div>
-     days ago 
+    ${timeago.format(tweet.created_at)}
     </div>
     <div class= 'tweet-reactions'>
     <i class="fa-brands fa-font-awesome"></i>
@@ -78,11 +78,11 @@ const createTweetElement = function (tweet) {
 };
 
 $(document).ready(function () {
-  renderTweets(data);
+  // renderTweets(data);
 
   $("#tweet-form").on("submit", function (event) {
     event.preventDefault();
-    
+
     $.ajax({
       url: "/tweets",
       type: "POST",
@@ -90,38 +90,21 @@ $(document).ready(function () {
       success: function (data, textStatus, jqXHR) {
         //data - response from server
         console.log(textStatus);
-        let tweetval = $("#tweet-text").val();
-
-        let text = `     <article class='tweets'>
-          <header class='tweet-header'>
-         
-           <div class='tweet-user'>
-         <img src="https://i.imgur.com/73hZDYK.png">
-            <div>name</div>
-            </div> 
-          <div class='user-handle'>handle</div>
-    
-          </header>
-    
-          <p class='tweet-body'> 
-            ${tweetval}
-          </p>
-    
-          <footer class='tweet-footer'>
-            <div>
-             days ago 
-            </div>
-            <div class= 'tweet-reactions'>
-            <i class="fa-brands fa-font-awesome"></i>
-            <i class="fa-solid fa-repeat"></i>
-            <i class="fa-solid fa-heart"></i>
-            </div>  
-          </footer>
-      </article>
-          `;
-        $("#tweets-container").prepend(text);
+        loadTweets();
       },
       error: function (jqXHR, textStatus, errorThrown) {},
     });
   });
+
+  const loadTweets = function () {
+    $.ajax({
+      url: "/tweets",
+      type: "GET",
+      success: function (data, textStatus, jqXHR) {
+        console.log(data);
+        renderTweets(data);
+      },
+    });
+  };
+  loadTweets();
 });
